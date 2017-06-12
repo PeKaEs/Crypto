7c0a9bfd0f8b649ab7a92e64d3eaaf545070fca3
PeKaEs <pekaes@protonmail.ch>
2017-06-12 17:38:30 +0200

    Merkle generating/encrypting/saving puzzle

--- lab4/MerkleCli.cpp
+++ lab4/MerkleCli.cpp
@@ -1,1 +1,30 @@
 #include "MerkleCli.hpp"
+
+/*
+Preamble reading
+temp[0] - > N
+temp[1] - > Broken Key Size [bits]
+temp[2] - > Keys Size [bits]
+temp[3] - > ID size [Bytes]
+temp[4] - > Const Value
+
+FILE *pEncRead = fopen(filePath.c_str(), "rb");
+    if (pEnc == NULL) {fputs ("Exange file fault, check path\n",stderr); exit (1);}
+uint temp[5];
+
+  fread(&temp, sizeof(uint), 5, pEncRead);
+for(uint i = 0; i < 5 ; ++i){
+  std::cout<<temp[i]<<std::endl;
+}
+
+Decrypt:
+
+AES_set_encrypt_key(ckey, 128, &encKey);
+num = 0;
+
+AES_cfb128_encrypt(toSave, finalRow, (4+ 4+ ( keysSize/8 )), &encKey, ivectest, &num, AES_DECRYPT);
+
+if(!fwrite(finalRow, 1, (4+ 4+ ( keysSize/8 )), pEnc)){
+    fputs ("Writing error",stderr); exit (5);}
+
+*/
--- lab4/MerkleSrv.cpp
+++ lab4/MerkleSrv.cpp
@@ -17,9 +17,9 @@ MerkleSrv::MerkleSrv(unsigned int sysPow, unsigned int keyS, unsigned int broken
     n = sysPow;
 
   puzzleSize = pow (2,n);
-  if (keyS > 256)
-    keysSize = 256;
-  else
+  //if (keyS > 256)
+    //keysSize = 256;
+  //else
   keysSize = keyS;
 
   if (brokenKeyS > 64)
@@ -54,48 +54,67 @@ void MerkleSrv::constructFinal(uint ID){
 
 void MerkleSrv::encAndSave(){
   int num = 0;
+  uint IDsize = 4;
+  uint uPreamble[5] = {n, brokenCryptoKeySize, keysSize, IDsize, constValue};
   AES_KEY encKey;
   FILE* pEnc;
-  unsigned char ivec[16];
-  unsigned char* ckey = new unsigned char[brokenCryptoKeySize/8];
+
+  unsigned char ivec[16],ckey[16] = {0};
+  unsigned char* toSave = new unsigned char[(uint)(4+ 4+ ( keysSize/8 ))];
+  unsigned long long int firIVpart, secIVpart;
 
   std::mt19937_64 genIV(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::mt19937 genKey(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<unsigned long long int> disIV(0x0, 0xFFFFFFFFFFFFFFFF);
-
-  unsigned long long int a = disIV(genIV);
-  unsigned long long int b = disIV(genIV)
-
-  memcpy(ivec ,&a,sizeof(a));
-  memcpy(ivec+sizeof(b) ,&b,sizeof(b));//if a = 0xA00000000000000B; and  b = 0xC00000000000000D; then ivec = 0B 00 00 00 00 00 00 A0 0D 00 00 00 00 00 00 C0
-
-  std::mt19937_64 genKey(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<unsigned char> disKey(0x0, 0xFF);
-
-  for( uint i = 0 ; i < (brokenCryptoKeySize/8) ; ++i ){
-    ckey[i] = disKey(genKey);
-  }
-
-
 
   pEnc = fopen(filePath.c_str(), "ab");
       if (pEnc == NULL) {fputs ("Exange file fault, check path\n",stderr); exit (1);}
 
-  //for(uint i = 0; i < puzzleSize; ++i){
-  //constructFinal(i);
-
-  if(!fwrite(ivec, 1, sizeof(ivec), pEnc)){
+//Preamble
+  if(!fwrite(&uPreamble, sizeof(uint), 5, pEnc)){
       fputs ("Writing error",stderr); exit (5);}
-  //}
-  //}
-/*
-  AES_set_encrypt_key(ckey, brokenCryptoKeySize, &encKey);
+//--Preamble
 
-  constructFinal(uint ID);
-  AES_cfb128_encrypt(finalRow, finalRow, (4+ 4+ ( keysSize/8 )), &encKey, ivec, &num, AES_ENCRYPT);
+uint perc = 0;
 
-  memcpy(ivec ,preIV.c_str(),sizeof(ivec));
-*/
-  delete ckey;
+  for(uint i = 0; i < puzzleSize; ++i){
+
+    constructFinal(valKeyPosPerm[i]);
+
+    if ( 2 * i > 300 )  //Prevents prediction of mt19937 values
+      genIV.seed(rd());
+    if ( (brokenCryptoKeySize/8) * i > 300 )  //Prevents prediction of mt19937 values
+      genKey.seed(rd());
+
+    firIVpart = disIV(genIV);
+    secIVpart = disIV(genIV);
+
+    memcpy(ivec ,&firIVpart,sizeof(firIVpart));
+    memcpy(ivec+sizeof(secIVpart) ,&secIVpart,sizeof(secIVpart));//if a = 0xA00000000000000B; and  b = 0xC00000000000000D; then ivec = 0B 00 00 00 00 00 00 A0 0D 00 00 00 00 00 00 C0
+
+    for( uint i = 15 ; i > 15 - (brokenCryptoKeySize/8) ; --i ){
+      ckey[i] = disKey(genKey);
+    }
+
+    AES_set_encrypt_key(ckey, 128, &encKey);
+
+    if(!fwrite(ivec, 1, 16, pEnc)){
+        fputs ("Writing error",stderr); exit (5);}
+
+    AES_cfb128_encrypt(finalRow, toSave, (4+ 4+ ( keysSize/8 )), &encKey, ivec, &num, AES_ENCRYPT);
+
+    if(!fwrite(toSave, 1, (4+ 4+ ( keysSize/8 )), pEnc)){
+        fputs ("Writing error",stderr); exit (5);}
+
+    num = 0;
+
+    if(i%(puzzleSize/10) == 0)
+      {std::cout<<"Encrypting/Saving keys: "<<perc<<"%"<<std::endl;perc+=10;}
+
+  }
+
+  delete toSave;
   fclose(pEnc);
 }
 
@@ -123,36 +142,17 @@ void MerkleSrv::makeKeys(){
   std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<unsigned char> dis(0x0, 0xFF);
 
+uint perc = 0;
   for( uint i = 0; i < puzzleSize; ++i ){
 
-    //valKeyMap.insert(std::pair<uint,std::vector<unsigned char>>(i,dis(gen)));
     if ( (keysSize/8) * i > 300 )
       gen.seed(rd());
 
-    //if(i%(puzzleSize/10) == 0)
-      //std::cout<<i<<std::endl;
+    if(i%(puzzleSize/10) == 0)
+      {std::cout<<"Creating keys: "<<perc<<"%"<<std::endl;perc+=10;}
 
     valKeyMap.insert( std::pair< uint, std::vector<unsigned char> > ( i, std::vector<unsigned char>() ) );
     for ( uint j = 0; j < keysSize/8; ++j )
         valKeyMap[i].push_back( dis(gen) );
   }
-/*
-  for(uint i = 0; i < puzzleSize; ++i){
-
-    for ( uint j = 0; j < keysSize/8; ++j )
-      std::cout<<valKeyMap[i][j];
-
-    std::cout<<""<<std::endl;
-  }
-
-  std::cout<<"\n"<<valKeyMap[1].size()<<"\n"<<std::endl;
-
-  for(uint i = 0; i < puzzleSize; ++i){
-
-    for ( uint j = 0; j < keysSize/8; ++j )
-        std::cout<<valKeyMap[valKeyPosPerm[i]][j];
-
-    std::cout<<""<<std::endl;
-  }
-  */
 }
